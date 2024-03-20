a2a8f3f5088c0d8052456a581f97ec14b435c10c
enzojgcaceres <enzojgcaceres@gmail.com>
2024-03-20 15:48:54 -0300

    Modelo de SALE

---
+++ src/models/sale/index.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+
+export const SaleModel = (sequelize) =>{
+    sequelize.define(
+        "Sale",
+        {
+            id:{
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            total: {
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                validate: {
+                    min: 0
+                }
+            },
+            date: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                validate:{
+                    isDate: true
+                }
+            },
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                // references: {
+                //     model: 'User', 
+                //     key: 'id'
+                // }
+            }
+        },
+        {
+            timestamps: false,
+        }
+    );
+};

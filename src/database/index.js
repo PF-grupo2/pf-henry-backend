5380078c2dc16f0bedf53fa9e1cde04b6b551e79
Enzo <136368220+enzojgcaceres@users.noreply.github.com>
2024-03-25 18:33:26 -0300

    Update index.js
    Corrección de ortografia - Sale x Sail

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import { POSTGRES_URI } from "../config/index.js";
 
-import { ProductModel, UserModel, ReviewModel, SaleDetailModel, SailModel } from "../models/index.js";
+import { ProductModel, UserModel, ReviewModel, SaleDetailModel, SaleModel } from "../models/index.js";
 
 const conn = new Sequelize(POSTGRES_URI, { logging: false, native: false });
 

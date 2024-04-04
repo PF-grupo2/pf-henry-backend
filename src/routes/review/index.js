d99e1da46afd13c9a0f74e4812a424aab7586af5
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-04-04 09:48:48 -0500

    Middleware Implementation

--- src/routes/review/index.js
+++ src/routes/review/index.js
@@ -1,11 +1,32 @@
 import { Router } from "express";
 import { reviewControllers } from "../../controllers/index.js";
 
+/*
+import {
+  jwtMiddlewares,
+  securityMiddlewares,
+} from "../../middlewares/index.js";
+*/
 const router = Router();
 
-router.get('/review', reviewControllers.getAllReviews);
-router.post('/review', reviewControllers.postReview);
-router.delete('/review/:id', reviewControllers.deleteReviews);
-router.put('/review/:id', reviewControllers.putReviews);
+router.get("/review", reviewControllers.getAllReviews);
+router.post("/review", reviewControllers.postReview);
+router.delete("/review/:id", reviewControllers.deleteReviews);
+router.put("/review/:id", reviewControllers.putReviews);
 
+// Middleware Implementation
+
+/*
+router.post("/review", jwtMiddlewares.validatJWT, reviewControllers.postReview);
+router.delete(
+  "/review/:id",
+  jwtMiddlewares.validatJWT,
+  reviewControllers.deleteReviews
+);
+router.put(
+  "/review/:id",
+  jwtMiddlewares.validatJWT,
+  reviewControllers.putReviews
+);
+*/
 export default router;
--- src/routes/sale/index.js
+++ src/routes/sale/index.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
 import { saleControllers } from "../../controllers/index.js";
 
+// import { jwtMiddlewares } from "../../middlewares/index.js";
+
 const router = Router();
 
 router.delete("/sale/id", saleControllers.deleteSale);
 router.post("/sale/id", saleControllers.postSale);
 router.get("/sale", saleControllers.getSale);
 router.put("/sale", saleControllers.putSale);
+
+/**
+ * 
+ * Middleware Implementation
+ * 
+ * 
+ *    router.delete("/sale/id",jwtMiddlewares.validatJWT, saleControllers.deleteSale);
+   router.post("/sale/id", jwtMiddlewares.validatJWT, saleControllers.postSale);
+   router.get("/sale", jwtMiddlewares.validatJWT,saleControllers.getSale);
+   router.put("/sale", jwtMiddlewares.validatJWT,saleControllers.putSale);
+ * 
+ */
 
 export default router;

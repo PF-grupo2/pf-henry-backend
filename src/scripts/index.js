0857d0e0c3b0499d51308bb488032b4d195ec183
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-04-14 13:22:22 -0500

    Fix Script Loader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,11 @@
-import axios from "axios";
 import { Product } from "../database/index.js";
+import { products } from "../config/data.js";
 
 export const loader = async () => {
   try {
     const productsDB = await Product.findAll({});
-    const { data } = await axios("http://localhost:5000/products");
-    if (productsDB.length < data.length) {
-      await Product.bulkCreate(data);
+    if (productsDB.length < products.length) {
+      await Product.bulkCreate(products);
     }
   } catch (error) {
     console.log(error);

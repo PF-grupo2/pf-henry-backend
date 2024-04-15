1f96faf885230b0a5cdd1a31cb6a5fc58719680b
cgcarlosgoni <gonicarlos11171@gmail.com>
2024-04-15 18:25:32 -0300

    fix/getProductByIdGetAllReviewPostReview

--- src/controllers/review/postReview.js
+++ src/controllers/review/postReview.js
@@ -1,17 +1,18 @@
 import { Review } from "../../database/index.js";
 
 const postReview = async (req, res) => {
-  const { score, message, status, ProductId, UserId } = req.body;
+  const { score, message, ProductId, UserId } = req.body;
   console.log(req.body);
   try {
-    if (!score || !message || !status || !ProductId || !UserId) throw Error({ error: "Missing information to create the review" });
+    if (!score || !message || !ProductId || !UserId) throw new Error("Missing information to create the review");
 
-    const data = { score, message, status, ProductId, UserId };
+    const data = { score, message, ProductId, UserId };
     console.log(data, "data");
     const newReview = await Review.create(data);
-    res.status(200).json(newReview);
-  } catch (error) {
-    res.status(404).json({ error: "The review was not created" });
+    return res.status(200).json(newReview);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ error: err.message });
   }
 };
 

8cff6e68bcab919aac0792b2f7578d9bc58bafb4
enzojgcaceres <enzojgcaceres@gmail.com>
2024-04-16 17:30:38 -0300

    Fix server cors policy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-// import multer from "multer";
 import router from "./routes/index.js";
 
 
@@ -10,23 +9,11 @@ const server = express();
 
 // Middlewares
 server.use(express.json());
-server.use(cors());
+server.use(cors({
+  origin: ["https://pf-henry-backend.onrender.com", "http://localhost:5173"],
+  credentials: true,
+}));
 server.use(morgan("dev"));
-
-server.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://pf-henry-backend.onrender.com/"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin,X-Requested-With,Content-Type,Accept,x-token"
-    );
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    next();
-  });
-
 
 
 // Route de la api

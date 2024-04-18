134b3ed6478399533e19e32dea29a75538452ab4
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-04-18 00:37:32 -0500

    Se reseteo la base de datos de usuarios para cargar la data de users

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,7 +8,21 @@ export const loader = async () => {
     if (productsDB.length < products.length) {
       await Product.bulkCreate(products);
     }
-    if (usersDB.length < users.length) {
+
+    if (users.find((user) => user.email === "Pomelo@gmail.com"))
+      await User.destroy();
+
+    if (usersDB.length > 0) {
+      for (let user of usersDB) {
+        const foundUser = users.find(
+          (currentUser) => currentUser.email === user.email
+        );
+        if (!foundUser) {
+          await usersDB.create(user);
+          await usersDB.save();
+        }
+      }
+    } else {
       await User.bulkCreate(users);
     }
   } catch (error) {

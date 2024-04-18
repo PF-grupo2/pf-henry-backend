1cc7c7c50450528087ab73dbe42a2a319940684a
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-04-18 03:36:35 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,14 +9,7 @@ const server = express();
 
 // Middlewares
 server.use(express.json());
-server.use(cors({
-  origin: [
-    "https://pf-henry-backend.onrender.com",
-     "http://localhost:5173",
-     "https://shoe-kingdom-ae164.web.app/",
-    ],
-  credentials: true,
-}));
+server.use(cors());
 server.use(morgan("dev"));
 
 
